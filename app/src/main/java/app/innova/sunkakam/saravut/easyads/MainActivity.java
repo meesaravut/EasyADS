ca729d49cb6163206a74075dca99e6715d6b7015
meeRD <slamdunkdota@hotmail.com>
2017-10-10 14:24:52 +0700

    add FM

--- app/src/main/java/app/innova/sunkakam/saravut/easyads/MainActivity.java
+++ app/src/main/java/app/innova/sunkakam/saravut/easyads/MainActivity.java
@@ -3,13 +3,28 @@ package app.innova.sunkakam.saravut.easyads;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import app.innova.sunkakam.saravut.easyads.fragment.Mainfragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null){
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragtMain, new Mainfragment()).commit();
+
+
+
+
+        }
     }//main method
+
+
+//    Add Fragment to activity
 
 
 }// main class
